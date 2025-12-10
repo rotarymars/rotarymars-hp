bcd9be06c80c15fd2cc4777ae7f76f05b32b48b3
rotarymars <s.goto2050@gmail.com>
2025-12-10 10:59:27 +0900

    new page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import styles from "@/styles/home.module.css"
 export default function Home() {
   return (
     <>
-      <h1>Hello world from rotarymars</h1>
+      <h1 className={styles.title}>Hello world from rotarymars</h1>
     </>
   )
 }
